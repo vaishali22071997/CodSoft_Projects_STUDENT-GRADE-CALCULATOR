4c05da178618fd6276e1a39631c83f6871287c35
vaishali22071997 <137603391+vaishali22071997@users.noreply.github.com>
2023-10-30 09:02:23 +0530

    Add files via upload

---
+++ StudentGradeCalculator/src/StudentGradeCalculator/StudentGradeCalculator.java
@@ -0,0 +1,46 @@
+package StudentGradeCalculator;
+
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+
+        double average = (double) totalMarks / numSubjects;
+        double percentage = (double) totalMarks / (numSubjects * 100) * 100;
+
+        char grade;
+
+        if (percentage >= 90) {
+            grade = 'A';
+        } else if (percentage >= 80) {
+            grade = 'B';
+        } else if (percentage >= 70) {
+            grade = 'C';
+        } else if (percentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Marks: " + average);
+        System.out.println("Percentage: " + percentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
+
